23e74c5fd60cad31847f7c765336a19d9e57b22d
Alex Volk <okamilex@gmail.com>
2016-03-22 19:32:34 +0300

    First Commit

---
+++ OkamiBooks/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OkamiBooks.MvcApplication" Language="C#" %>
---
+++ OkamiBooks/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OkamiBooks.Startup))]
+namespace OkamiBooks
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
